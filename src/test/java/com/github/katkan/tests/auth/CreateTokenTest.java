e6159c55f098263db980cb96176252cc43b9d687
Katarzyna <kankowska.katarzyna@gmail.com>
2022-10-20 19:22:26 +0200

    Add AUTH (Create Token) tests

---
+++ src/test/java/com/github/katkan/tests/auth/CreateTokenTest.java
@@ -0,0 +1,45 @@
+package com.github.katkan.tests.auth;
+
+import com.github.katkan.properties.RestfulBookerProperties;
+import com.github.katkan.requests.auth.CreateTokenRequest;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.github.katkan.helpers.JsonHelper.PASSWORD;
+import static com.github.katkan.helpers.JsonHelper.USERNAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CreateTokenTest {
+    private JSONObject authData;
+
+    @BeforeEach
+    void setup(){
+        authData = new JSONObject();
+    }
+
+    @Test
+    @DisplayName("Create token with valid username and password")
+    void createTokenWithValidUsernameAndPassword() {
+        authData.put(USERNAME, RestfulBookerProperties.getUsername());
+        authData.put(PASSWORD, RestfulBookerProperties.getPassword());
+        Response createTokenResponse = CreateTokenRequest.createTokenRequest(authData);
+
+        assertThat(createTokenResponse.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+        assertThat(createTokenResponse.jsonPath().getString("token")).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("Create token with invalid username and password")
+    void createTokenWithInvalidUsernameAndPassword() {
+        authData.put(USERNAME, "user");
+        authData.put(PASSWORD, "123123");
+        Response createTokenResponse = CreateTokenRequest.createTokenRequest(authData);
+
+        assertThat(createTokenResponse.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+        assertThat(createTokenResponse.jsonPath().getString("reason")).isEqualTo("Bad credentials");
+    }
+}
